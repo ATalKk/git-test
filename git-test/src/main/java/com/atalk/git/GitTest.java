14d46832d60f9818199c1be732d42f59df1d8ee2
ATalK <ATalK@qq.com>
2022-12-04 11:43:42 +0800

    first commit

---
+++ git-test/src/main/java/com/atalk/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atalk.git;
+
+/**
+ * @author ATalK
+ * @create 2022-12-04 上午 11:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
